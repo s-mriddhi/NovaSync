d7fedfee4a923639aefa77d217c4ac5320a6f115
Samriddhi Singh <pabbucooldude@Samriddhis-MacBook-Pro.local>
2025-12-05 12:23:38 +0530

    In Expense_Splits db share of each share is now calculated in the same create expense request with splits in the owed_Amount column, see if this can be renamed in db; TBD: Net value for each user ka column to be created in the db

--- backend/src/models/expenseSplit.model.js
+++ backend/src/models/expenseSplit.model.js
@@ -1,11 +1,11 @@
 //expenseSplit.model.js
 import pool from '../config/db.js';
 
-export const createExpenseSplit = async (expenseId, userId, owed_Amount, splitType, value) => {
+export const createExpenseSplit = async (expenseId, userId, owedAmount, splitType, value) => {
   const result = await pool.query(
     `INSERT INTO expense_splits (expense_id, user_id, owed_amount, split_type, value)
      VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-    [expenseId, userId, 0, splitType, value]
+    [expenseId, userId, owedAmount, splitType, value]
   );
   return result.rows[0];
 };
